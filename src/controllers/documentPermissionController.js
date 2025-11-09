fc8d78ff98936a8cd1783d69a02acd456442c866
Foodevn <hoangphuc0918065630@gmail.com>
2025-11-09 09:18:53 +0700

    feat: implement addUser, updatePermission, and deletePermission routes in documentPermissionController

--- src/controllers/documentPermissionController.js
+++ src/controllers/documentPermissionController.js
@@ -2,7 +2,6 @@ import { db } from "../db/index.js";
 import { and, eq, inArray } from "drizzle-orm";
 import { documentsPermissions, users } from "../db/schema.js";
 
-
 export const checkPermission = async (req, res) => {
     try {
         const { documentId } = req.body;
@@ -36,6 +35,7 @@ export const checkPermission = async (req, res) => {
         return res.status(500).json({ message: "Lỗi hệ thống" });
     }
 }
+
 export const getDocumentsPermissions = async (req, res) => {
     try {
         const { documentId } = req.params;
@@ -58,7 +58,6 @@ export const getDocumentsPermissions = async (req, res) => {
             .from(documentsPermissions)
             .innerJoin(users, eq(documentsPermissions.userId, users.id))
             .where(eq(documentsPermissions.documentId, documentId))
-        console.log(documentPermissions);
 
         return res.status(200).json({
             documentPermissions
@@ -69,4 +68,151 @@ export const getDocumentsPermissions = async (req, res) => {
         return res.status(500).json({ message: "Lỗi hệ thống" });
     }
 
+}
+
+export const addUser = async (req, res) => {
+    try {
+        const { email, documentId } = req.body;
+        if (!email) {
+            return res.status(400).json({ message: "email là bắt buộc" });
+        }
+        //tìm người dùng theo email của người dùng
+        const user = await db.select().from(users).where(eq(users.email, email));
+        if (!user[0]) {
+            return res.status(404).json({ message: 'email không tồn tại' })
+        }
+
+        //kiểm tra xem quyền này đã có chưa
+        const checkExisting = await db.select().from(documentsPermissions).where(
+            and(
+                eq(documentsPermissions.userId, user[0].id),
+                eq(documentsPermissions.documentId, documentId)
+            )
+        )
+        if (checkExisting[0]) {
+            return res.status(400).json({ message: "email đã có quyền trong tài liệu" })
+        }
+
+        //lấy id của người dùng cùng với id của tài liệu thêm bảng ghi mới với quyền chỉ xem
+        const addPermission = await db.insert(documentsPermissions)
+            .values({
+                userId: user[0].id,
+                documentId: documentId,
+                permission: 'viewer',
+            }).returning();
+
+        //trả về id người dùng
+        return res.status(200).json({
+            userId: addPermission[0].userId,
+        })
+    } catch (error) {
+        console.error("Lỗi khi gọi documents permission", error);
+        return res.status(500).json({ message: "Lỗi hệ thống" });
+    }
+}
+
+export const updatePermission = async (req, res) => {
+    try {
+        const { documentId } = req.params;
+
+
+
+
+        return res.status(200).json({
+
+        })
+    } catch (error) {
+        console.error("Lỗi khi gọi documents permission", error);
+        return res.status(500).json({ message: "Lỗi hệ thống" });
+    }
+}
+
+export const deletePermission = async (req, res) => {
+    try {
+        const { documentId } = req.params;
+        const userId = req.body.userId;
+        const currentUser = req.user;
+
+        if (!userId) {
+            return res.status(400).json({
+                message: "thiếu user id",
+            })
+        }
+        //Kiểm tra quyền của người thực hiện hành động xóa
+        const currentUserPermission = await db
+            .select()
+            .from(documentsPermissions)
+            .where(
+                and(
+                    eq(documentsPermissions.documentId, documentId),
+                    eq(documentsPermissions.userId, currentUser.id),
+                    eq(documentsPermissions.permission, "admin"),
+                )
+            )
+            .limit(1);
+
+        if (!currentUserPermission[0]) {
+            return res.status(403).json({
+                message: "Bạn không có quyền truy cập tài liệu này",
+            });
+        }
+
+        //kiem tra co quyen nay chua
+        const existingPermission = await db
+            .select()
+            .from(documentsPermissions)
+            .where(
+                and(
+                    eq(documentsPermissions.documentId, documentId),
+                    eq(documentsPermissions.userId, userId),
+                )
+            )
+            .limit(1);
+
+        if (!existingPermission[0]) {
+            return res.status(404).json({
+                message: "Quyền này không tồn tại hoặc đã xóa",
+            })
+        }
+
+        // Không cho phép xóa chính mình (admin cuối cùng)
+        if (userId === currentUser.id) {
+            // Kiểm tra xem còn admin nào khác không
+            const allAdmins = await db
+                .select()
+                .from(documentsPermissions)
+                .where(
+                    and(
+                        eq(documentsPermissions.documentId, documentId),
+                        eq(documentsPermissions.permission, 'admin')
+                    )
+                );
+
+            if (allAdmins.length === 1) {
+                return res.status(400).json({
+                    message: "Không thể xóa admin cuối cùng của tài liệu",
+                });
+            }
+        }
+
+
+
+        // XÓA permission
+        await db
+            .delete(documentsPermissions)
+            .where(
+                and(
+                    eq(documentsPermissions.documentId, documentId),
+                    eq(documentsPermissions.userId, userId),
+                )
+            );
+
+        //Xóa thành công
+        return res.status(200).json({
+            message: "Xóa quyền thành công",
+        });
+    } catch (error) {
+        console.error("Lỗi khi gọi documents permission", error);
+        return res.status(500).json({ message: "Lỗi hệ thống" });
+    }
 }
--- src/routes/documentPermissionRoute.js
+++ src/routes/documentPermissionRoute.js
@@ -2,12 +2,16 @@ import express from "express";
 import {
     checkPermission,
     getDocumentsPermissions,
-
+    addUser,
+    updatePermission,
+    deletePermission,
 } from "../controllers/documentPermissionController.js";
 
 const router = express.Router();
 
 router.post("/check", checkPermission);
 router.get("/id/:documentId", getDocumentsPermissions);
-
+router.post("/add", addUser)
+router.put("/:documentId", updatePermission);
+router.delete("/:documentId", deletePermission);
 export default router;
